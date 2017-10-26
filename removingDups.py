659cbc20df0fa770f9466fbb5fe24aabf53ee05d
Colby Witherup <colbywitherup@gmail.com>
2017-10-25 20:13:48 -0500

    changed to take a path/gene as only argument so can be used with gnu parallel

--- removingDups.py
+++ removingDups.py
@@ -2,21 +2,26 @@ from ete3 import PhyloTree
 import re
 import os
 
+###This script removes within species duplications. 
+###sys.argv[1] is path to a tree file called {}.fa.tre.
+###It's also path to where the output tree will be made called {}.2.tre
+
 def main():
-	with open(sys.arg[1], "r") as f:
-		orthogroup_list=[line.rstrip() for line in f.readlines()]
-	for i in orthogroup_list:
-		copy_list=remove_species_dups(i)
-		species_set=make_species_set(i)
-		if len(copy_list)==len(species_set):
-			print("{} is SINGLE".format(i))
-		else:
-			diff=len(copy_list)-len(species_set)
-			print("For {} there are {} remaining duplicate species".format(i,diff))
+	with open("{}.fa.tre".format(sys.arg[1]), "r") as f:
+	copy_list=remove_species_dups(sys.argv[1])
+	species_set=make_species_set(sys.argv[1])
+	if len(copy_list)==len(species_set):
+		print("{} is SINGLE".format(i))
+	else:
+		diff=len(copy_list)-len(species_set)
+		print("For {} there are {} remaining duplicate species".format(i,diff))
+		species_copy_list=[re.sub("\d","",j) for j in copy_list]
+		for x in species_set:
+			if species_copy_list.count(x)>1:
+				print("Extra copy is in {}".format(x))
 
-
-def make_species_set(gene):
-	t = PhyloTree("/Users/colbyjones/Documents/singleCopy/genes/{}/{}.fa.tre".format(gene,gene))
+def make_species_set(path):
+	t = PhyloTree("{}.fa.tre".format(path))
 	leaves=[]
 	for leaf in t:
 		leaves.append(leaf)
@@ -27,9 +32,8 @@ def make_species_set(gene):
 	
 	return(set_l)
 
-
-def remove_species_dups(gene):
-	t = PhyloTree("/Users/colbyjones/Documents/singleCopy/genes/{}/{}.fa.tre".format(gene,gene))
+def remove_species_dups(path):
+	t = PhyloTree("{}.fa.tre".format(path))
 	t.set_species_naming_function(lambda node: re.sub("\d","",node.name))
 
 	t2 = t.collapse_lineage_specific_expansions() 
