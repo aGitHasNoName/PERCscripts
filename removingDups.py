306daa345f552914452d8e7eae41cb530042fc53
Colby Witherup <colbywitherup@gmail.com>
2017-10-25 20:32:10 -0500

    added writing .txt or .tre file

--- removingDups.py
+++ removingDups.py
@@ -7,18 +7,18 @@ import os
 ###It's also path to where the output tree will be made called {}.2.tre
 
 def main():
-	with open("{}.fa.tre".format(sys.arg[1]), "r") as f:
-	copy_list=remove_species_dups(sys.argv[1])
-	species_set=make_species_set(sys.argv[1])
+	path=sys.argv[1]
+	copy_list,t2=remove_species_dups(path)
+	species_set=make_species_set(path)
 	if len(copy_list)==len(species_set):
-		print("{} is SINGLE".format(i))
+		print("{} is SINGLE".format(path))
+		with open("{}.2.fa.tre".format(path),"w") as f:
+			for i in copy_list:
+				f.write("{}\n".format(i))
 	else:
 		diff=len(copy_list)-len(species_set)
-		print("For {} there are {} remaining duplicate species".format(i,diff))
-		species_copy_list=[re.sub("\d","",j) for j in copy_list]
-		for x in species_set:
-			if species_copy_list.count(x)>1:
-				print("Extra copy is in {}".format(x))
+		print("For {} there are {} remaining duplicate species".format(path,diff))
+		t2.write("{}.2.fa.tre".format(path),"w")
 
 def make_species_set(path):
 	t = PhyloTree("{}.fa.tre".format(path))
@@ -29,19 +29,16 @@ def make_species_set(path):
 	l=[i.lstrip("\n--") for i in l]
 	l=[re.sub("\d","",i) for i in l]
 	set_l=set(l)
-	
 	return(set_l)
 
 def remove_species_dups(path):
 	t = PhyloTree("{}.fa.tre".format(path))
 	t.set_species_naming_function(lambda node: re.sub("\d","",node.name))
-
 	t2 = t.collapse_lineage_specific_expansions() 
 	leaves2=[]
 	for leaf in t2:
 		leaves2.append(leaf)
 	l2=[str(i) for i in leaves2]
 	l2=[i.lstrip("\n--") for i in l2]
-
-	return(l2)
+	return(l2,t2)
 
