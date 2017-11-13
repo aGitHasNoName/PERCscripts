b83161a9a86637b21f74e4428a2a6fb313d5fe92
Colby Witherup <colbywitherup@gmail.com>
2017-11-13 13:59:10 -0600

    added function for removing sequences that are <25% the median length of all sequences

--- removingDups.py
+++ removingDups.py
@@ -1,18 +1,20 @@
 from ete3 import PhyloTree
 import re
+import statistics
 import os
 import sys
+from Bio import SeqIO
 
 ###This script removes within species duplications. 
-###sys.argv[1] is path to a tree file called {}.fa.tre.
+###sys.argv[1] is path to a tree file called {}.fa.tre and alignment file called {}.fa.aln.
 ###It's also path to where the output tree will be made called {}.2.tre
 
 def main():
 	path=sys.argv[1]
 	t = PhyloTree("{}.fa.tre".format(path))
-	species_set=make_species_set(path,t)
-	t=removeShort(t)
-	copy_list,t2=remove_species_dups(path,t)
+	species_set=make_species_set(t)
+	t2=removeShort(t,path)
+	copy_list,t3=remove_species_dups(t2)
 	if len(copy_list)==len(species_set):
 		print("{} is SINGLE".format(path))
 		with open("{}_single.txt".format(path),"w") as f:
@@ -21,8 +23,23 @@ def main():
 	else:
 		diff=len(copy_list)-len(species_set)
 		print("For {} there are {} remaining duplicate species".format(path,diff))
-		t2.write(outfile="{}.2.fa.tre".format(path))
+		t3.write(outfile="{}.2.fa.tre".format(path))
 
+def removeShort(t,path):
+	print("Filtering out duplicate sequences that are <25% the average length for all sequences...")
+	with open("{}.fa.aln".format(path), "r") as f:
+		aa_list=[str(record.seq) for record in SeqIO.parse(f, "fasta")]
+		aaLen_list=map(lambda x: len(re.sub("\-","",x)),aa_list)
+		aaLen_median=statistics.median(aaLen_list)
+	keep_list=[]
+	with open("{}.fa.aln".format(path), "r") as f:	
+		for record in SeqIO.parse(f, "fasta"):
+			aa=re.sub("\-","",record.seq)
+			if len(aa) >.3*aaLen_median:
+				keep_list.append(record.id)
+	t.prune(keep_list,preserve_branch_length=True)
+	return(t)
+	
 def make_species_set(t):
 	leaves=[]
 	for leaf in t:
