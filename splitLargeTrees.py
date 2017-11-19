35203df4cd0a443f92a438b4c52c3baa318f0c74
Colby Witherup <colbywitherup@gmail.com>
2017-11-19 13:26:54 -0600

    condensed part of the yes choice function in its own function, fixed todo to delete completed genes from file

--- splitLargeTrees.py
+++ splitLargeTrees.py
@@ -29,6 +29,20 @@ def yes_choice(tree_file_name, gene):
 	group_list = clade_to_tree(t)
 	###tree1
 	cut_list = [i for i in gene_names if i not in group_list]
+	gene1 = yesMake(cut_list, gene, tree_file_name)
+	###tree2
+	cut_list1 = [i for i in gene_names if i not in cut_list]
+	gene2 = yesMake(cut_list1, gene1, tree_file_name)
+	with open(sys.argv[2], "r") as f:
+		todo_list=[line.rstrip() for line in f]
+	todo_list=[i for i in todo_list if i != gene]
+	todo_list.append(gene1)
+	todo_list.append(gene2)
+	with open(sys.argv[2], "w") as todo:
+		for i in todo_list:
+			todo.write(i+"\n")
+
+def yesMake(cut_list, gene, tree_file_name):
 	tree=PhyloTree(tree_file_name)
 	tree.prune(cut_list, preserve_branch_length=True)
 	if gene[-3] == "_":
@@ -40,23 +54,7 @@ def yes_choice(tree_file_name, gene):
 	directory = ("{}/{}".format(sys.argv[1], gene1))
 	os.system("mkdir {}".format(directory))
 	tree.write(format=1, outfile=new_file)
-	with open(sys.argv[2], "a") as todo:
-		todo.write("\n{}".format(gene1))
-	###tree2
-	cut_list2 = [i for i in gene_names if i not in cut_list]
-	tree2=PhyloTree(tree_file_name)
-	tree2.prune(cut_list2, preserve_branch_length=True)
-	if gene1[-3] == "_":
-		n = int(gene1[-1])+1
-		gene2 = gene1[:-1]+str(n)
-	else:
-		gene2 = gene1+"_10"
-	directory2 = ("{}/{}".format(sys.argv[1], gene2))
-	os.system("mkdir {}".format(directory2))
-	new_file2 = "{}/{}/{}.3.fa.tre".format(sys.argv[1], gene2, gene2)
-	tree2.write(format=1, outfile=new_file2)
-	with open(sys.argv[2], "a") as todo:
-		todo.write("\n{}".format(gene2))
+	return(gene1)
 
 def no_choice(gene):
 	with open(sys.argv[3], "a") as master:
